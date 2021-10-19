8239c7261331a86fb8022b6b8edb3fde350c26a5
hel1yeah <hel1_yeah@ukr.net>
2021-10-19 18:41:36 +0300

    feat: added fonts

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import store from "./store";
 
+import "./assets/scss/main.scss";
+
 const app = createApp(App);
 
 app.use(store).mount("#app");
